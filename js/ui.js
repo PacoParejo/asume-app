574d22f737e1fa88d5e43894555dada2dbbb4f3f
PacoParejo <67227900+PacoParejo@users.noreply.github.com>
2026-04-18 23:03:06 +0200

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,7 @@
+export function setView(title, html) {
+  const viewTitle = document.getElementById('viewTitle');
+  const viewContent = document.getElementById('viewContent');
+
+  if (viewTitle) viewTitle.textContent = title;
+  if (viewContent) viewContent.innerHTML = html;
+}
